1e9184012652a1c2bcef71f054f81c4be6c38e2e
youiecho <minyounc@Andrew.cmu.edu>
2021-11-30 21:41:48 -0500

    main grid and animation structures

--- main.js
+++ main.js
@@ -1,10 +1,101 @@
-const btn1 = document.getElementById('btn1');
+/* Background color changes */
 
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
+function bgColorChange() {
+    const btn1 = document.getElementById('btn1');
+    btn1.onclick = function () {
+        const colChange1 = 'rgb(231, 191, 255)';
+        document.body.style.backgroundColor = colChange1;
+    }
+
+    const btn2 = document.getElementById('btn2');
+    btn2.onclick = function () {
+        const colChange2 = 'rgb(255, 191, 225)';
+        document.body.style.backgroundColor = colChange2;
+    }
+
+    const btn3 = document.getElementById('btn3');
+    btn3.onclick = function () {
+        const colChange3 = 'rgb(255, 198, 191)';
+        document.body.style.backgroundColor = colChange3;
+    }
 }
 
-btn1.onclick = function () {
-    const colChange1 = 'rgb(' + random(100, 255) + ',' + random(150, 255) + ',' + random(200, 255) + ')';
-    document.body.style.backgroundColor = colChange1;
+$('.behaviorImg').click(function () {
+    $('#insert').addClass('pullDown');
+});
+
+var $post = $("#insert");
+setInterval(function () {
+    $post.removeClass("pullDown");
+}, 1000);
+
+function one() {
+    var learnMoreContainer = document.getElementById("insert");
+    var button = document.createElement("button");
+    learnMoreContainer.innerHTML = "this iscontent a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a ";
+    button.innerHTML = "Learn More";
+    learnMoreContainer.append(button);
+    button.addEventListener('click', expandLearnMore);
+    function expandLearnMore() {
+        console.log("hi");
+        learnMoreContainer.setAttribute('class', 'animate__animated animate__bounce');
+    }
 }
+
+function two() {
+    var add = document.getElementById("insert");
+    add.innerHTML = "two";
+}
+
+function three() {
+    var add = document.getElementById("insert");
+    add.innerHTML = "three";
+}
+
+
+
+
+$(window).scroll(function () {
+    $('.question2').each(function () {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("animate__animated animate__bounce");
+        }
+    });
+});
+
+$(window).scroll(function () {
+    $('.question3').each(function () {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("animate__animated animate__bounce");
+        }
+    });
+});
+
+$(window).scroll(function () {
+    $('.button2').each(function () {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("animate__animated animate__fadeInUp");
+        }
+    });
+});
+
+$(window).scroll(function () {
+    $('.button3').each(function () {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("animate__animated animate__fadeInUp");
+        }
+    });
+});
+
